419dabfc904c81404133fa51898f704cffbc02a1
illua <gyeonju_ksk@naver.com>
2024-01-17 07:11:05 +0900

    init

--- src/pages/home/test2.tsx
+++ src/pages/home/test2.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, List, ListItem, Typography, Container } from '@mui/material';
 import styles from '../../styles/Home.module.css'; // Import your styles
 
 const DualListSelection = () => {
@@ -8,35 +9,27 @@ const DualListSelection = () => {
         'Item 2': ['Subitem X', 'Subitem Y', 'Subitem Z'],
         'Item 3': ['Subitem P', 'Subitem Q', 'Subitem R'],
     };
-    const jobList = ['Job A', 'Job B', 'Job C', 'Job D'];
+    const list3Items = ['Item X', 'Item Y', 'Item Z'];
 
     const [selectedList1Item, setSelectedList1Item] = useState(null);
     const [selectedList2Item, setSelectedList2Item] = useState(null);
-    const [selectedJob, setSelectedJob] = useState(null);
     const [scrollList, setScrollList] = useState([]);
 
     const handleList1Selection = (item) => {
         setSelectedList1Item(item);
         setSelectedList2Item(null);
-        setSelectedJob(null);
+        addToScrollList(list1Items.indexOf(item)); // Add corresponding item from List 3
     };
 
     const handleList2Selection = (item) => {
         setSelectedList2Item(item);
-        setSelectedJob(null);
+        addToScrollList(list2Items[selectedList1Item].indexOf(item)); // Add corresponding item from List 3
     };
 
-    const handleJobSelection = (job) => {
-        setSelectedJob(job);
-        setSelectedList1Item(null);
-        setSelectedList2Item(null);
-    };
-
-    const addToScrollList = () => {
-        if (selectedList2Item && !scrollList.includes(selectedList2Item)) {
-            setScrollList((prevList) => [...prevList, selectedList2Item]);
-        } else if (selectedJob && !scrollList.includes(selectedJob)) {
-            setScrollList((prevList) => [...prevList, selectedJob]);
+    const addToScrollList = (index) => {
+        const newItem = list3Items[index];
+        if (!scrollList.includes(newItem)) {
+            setScrollList((prevList) => [...prevList, newItem]);
         }
     };
 
@@ -49,76 +42,62 @@ const DualListSelection = () => {
     };
 
     return (
-        <div className={styles.dualListContainer}>
-            <div className={styles.listContainer}>
-                <h2>List 1</h2>
-                <ul>
-                    {list1Items.map((item) => (
-                        <li
+        <Container maxWidth="md" className={styles.dualListContainer}>
+            <List>
+                <Typography variant="h6">List 1</Typography>
+                {list1Items.map((item) => (
+                    <ListItem
+                        key={item}
+                        onClick={() => handleList1Selection(item)}
+                        selected={selectedList1Item === item}
+                        button
+                    >
+                        {item}
+                    </ListItem>
+                ))}
+                <Typography>Selected Item in List 1: {selectedList1Item}</Typography>
+            </List>
+
+            <List>
+                <Typography variant="h6">List 2</Typography>
+                {selectedList1Item &&
+                    list2Items[selectedList1Item].map((item) => (
+                        <ListItem
                             key={item}
-                            onClick={() => handleList1Selection(item)}
-                            className={
-                                selectedList1Item === item ? styles.selected : styles.item
-                            }
+                            onClick={() => handleList2Selection(item)}
+                            selected={selectedList2Item === item}
+                            button
                         >
                             {item}
-                        </li>
+                        </ListItem>
                     ))}
-                </ul>
-                <p>Selected Item in List 1: {selectedList1Item}</p>
-            </div>
+                <Typography>Selected Item in List 2: {selectedList2Item}</Typography>
+                <Button variant="contained" onClick={addToScrollList}>
+                    Add to Scroll List
+                </Button>
+            </List>
 
-            <div className={styles.listContainer}>
-                <h2>List 2</h2>
-                <ul>
-                    {selectedList1Item &&
-                        list2Items[selectedList1Item].map((item) => (
-                            <li
-                                key={item}
-                                onClick={() => handleList2Selection(item)}
-                                className={
-                                    selectedList2Item === item ? styles.selected : styles.item
-                                }
-                            >
-                                {item}
-                            </li>
-                        ))}
-                </ul>
-                <p>Selected Item in List 2: {selectedList2Item}</p>
-                <button onClick={addToScrollList}>Add to Scroll List</button>
-            </div>
+            <List>
+                <Typography variant="h6">List 3</Typography>
+                {list3Items.map((item) => (
+                    <ListItem key={item} onClick={() => addToScrollList(list3Items.indexOf(item))} button>
+                        {item}
+                    </ListItem>
+                ))}
+            </List>
 
-            <div className={styles.listContainer}>
-                <h2>Job List</h2>
-                <ul>
-                    {jobList.map((job) => (
-                        <li
-                            key={job}
-                            onClick={() => handleJobSelection(job)}
-                            className={
-                                selectedJob === job ? styles.selected : styles.item
-                            }
-                        >
-                            {job}
-                        </li>
-                    ))}
-                </ul>
-                <p>Selected Job: {selectedJob}</p>
-                <button onClick={addToScrollList}>Add to Scroll List</button>
-            </div>
-
-            <div className={styles.scrollListContainer}>
-                <h2>Scroll List</h2>
-                <ul className={styles.scrollList}>
-                    {scrollList.map((item) => (
-                        <li key={item} onClick={() => removeFromScrollList(item)}>
-                            {item}
-                        </li>
-                    ))}
-                </ul>
-                <button onClick={clearScrollList}>Clear Scroll List</button>
-            </div>
-        </div>
+            <List>
+                <Typography variant="h6">Scroll List</Typography>
+                {scrollList.map((item) => (
+                    <ListItem key={item} onClick={() => removeFromScrollList(item)} button>
+                        {item}
+                    </ListItem>
+                ))}
+                <Button variant="contained" onClick={clearScrollList}>
+                    Clear Scroll List
+                </Button>
+            </List>
+        </Container>
     );
 };
 
